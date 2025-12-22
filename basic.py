ba3417f9d34f62b738bd069a22e4ce56d5ee8ca6
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2025-12-23 00:09:50 +0530

    adding Basic file

---
+++ basic.py
@@ -0,0 +1,33 @@
+"""
+1. Write a python program to add two numbers.
+
+2. Write a python program to find remainder when a number is divided by z.
+
+3. Check the type of variable assigned using input () function.
+
+4. Use comparison operator to find out whether a given variable a is greater than 'b' or not. Take a = 34 and b = 80
+
+5. Write a python program to find an average of two numbers entered by the user.
+
+6. Write a python program to calculate the square of a number entered by the user."""
+
+# 1. Write a python program to add two numbers.
+a= 10 
+b= 20
+print("Sum of two numbers:", a + b)
+
+# 2. Write a python program to find remainder when a number is divided by z.
+num1 = 29
+num2 = 5
+remainder = num1 % num2
+print("Remainder when", num1, "is divided by", num2, "is:", remainder)
+
+# 3. Check the type of variable assigned using input () function.
+user_input = input("Enter something: ")
+print("The type of the variable assigned using input() function is:", type(user_input))
+
+# 4. Use comparison operator to find out whether a given variable a is greater than 'b' or not. Take a = 34 and b = 80
+a = 34
+b = 80 
+is_a_greater_than_b = a > b
+print("Is a greater than b?:", is_a_greater_than_b)
