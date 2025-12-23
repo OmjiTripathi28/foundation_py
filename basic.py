2d2f204b830c3ee2d6bd58d3ec2492c15413fcb0
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2025-12-23 10:38:30 +0530

    added 3 question

--- basic.py
+++ basic.py
@@ -27,7 +27,18 @@ user_input = input("Enter something: ")
 print("The type of the variable assigned using input() function is:", type(user_input))
 
 # 4. Use comparison operator to find out whether a given variable a is greater than 'b' or not. Take a = 34 and b = 80
-a = 34
-b = 80 
-is_a_greater_than_b = a > b
-print("Is a greater than b?:", is_a_greater_than_b)
+numA = 34
+numB = 80
+is_greater= numA > numB
+print("A is greater than B: ", is_greater)
+
+#5. Write a python program to find an average of two numbers entered by the user.
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter another number: "))
+avg = (num3+num4)/2
+print("Average of numbers you entered is :",avg)
+
+#6. Write a python program to calculate the square of a number entered by the user.
+num5 = int(input("Enter a num :"))
+sq = num5*num5
+print("square of the number is : ",sq)
