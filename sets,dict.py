e1e46344ee9b3c8c724788507ce4b315dc04f0ec
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-03 23:53:34 +0530

    added

--- sets,dict.py
+++ sets,dict.py
@@ -30,3 +30,18 @@ print(statement.replace("  "," "))
 
 #5.Write a program to format the following letter using escape sequence characters. Letter = "Dear Harry, this python course is nice. Thanks!
 print("Dear omii, \nI'm fine.\nThanks!")
+
+#1.Write a python program to display a user entered name followed by Good Afternoon using input () function.
+name = input ("Enter you name :")
+print("Greetings," ,name)
+
+#2.Write a program to fill in a letter template given below with name and date.
+Name = input("Enter you name :")
+Date = input("Enter date :")
+print(f"""Dear {Name},
+You are selected!
+{Date}""")
+
+#3.Write a program to detect double space in a string. 
+statement = "This string contains  double spaces."
+print(statement.find("  "))
