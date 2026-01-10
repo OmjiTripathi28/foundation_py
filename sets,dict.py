06ef56d10ee0e04f1c2932fd0a1d9bf4fa25458e
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-10 23:52:13 +0530

    solved1

--- sets,dict.py
+++ sets,dict.py
@@ -24,24 +24,12 @@ S = {} What is the type of S?
 8.Can you change the values inside a list which is contained in set S? S = {8, 7, 12, "Harry", [1,2]}"""
 
 #1.Write a program to create a dictionary of Hindi words with values as their English translation. 
-# Provide user with an option to look it up!
-#4.Replace the double space from problem 3 with single spaces.
-print(statement.replace("  "," "))
+words = {
+    "madad"  : "help",
+    "kurshi" : "chair",
+    "kitaab" : "book"
+}
 
-#5.Write a program to format the following letter using escape sequence characters. Letter = "Dear Harry, this python course is nice. Thanks!
-print("Dear omii, \nI'm fine.\nThanks!")
+word = input("enter the word you want the meaning of : ")
+print(words[word])
 
-#1.Write a python program to display a user entered name followed by Good Afternoon using input () function.
-name = input ("Enter you name :")
-print("Greetings," ,name)
-
-#2.Write a program to fill in a letter template given below with name and date.
-Name = input("Enter you name :")
-Date = input("Enter date :")
-print(f"""Dear {Name},
-You are selected!
-{Date}""")
-
-#3.Write a program to detect double space in a string. 
-statement = "This string contains  double spaces."
-print(statement.find("  "))
