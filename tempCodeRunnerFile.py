de6ddc286a92d73887de7bf7f3553ded9532f31c
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-11 09:24:35 +0530

    solved3

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+#1.Write a program to create a dictionary of Hindi words with values as their English translation. 
+words = {
+    "madad"  : "help",
+    "kurshi" : "chair",
+    "kitaab" : "book"
+}
+
+word = input("enter the word you want the meaning of : ")
+print(words[word])
+
+#2 Write a program to input eight numbers from the user and display all the unique numbers (once).
+nums = set()
+for i in range (0,7):
+    num = int(input("enter a number you want in a set: "))
+    nums.add(num)
+
+print(nums)
