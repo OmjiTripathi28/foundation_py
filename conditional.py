a3f5839a95f6cea18083603d23490da6f62a2db9
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-12 18:11:16 +0530

    solved1

--- conditional.py
+++ conditional.py
@@ -23,3 +23,17 @@ following scheme:
 <50 => F
 
 7. Write a program to find out whether a given post is talking about “Harry” or not"""
+
+#1Write a program to find the greatest of four numbers entered by the user.
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+c = int(input("Enter a number:"))
+d = int(input("Enter a number:"))
+if a>b & a>c & a>d:
+    print("a is the greatest of all")
+elif b>a & b>c & b>d:
+    print("b is the greatest of all")
+elif c>a & c>b & c>d:
+    print("c is the greatest of all")
+else :
+    print("d is the greatest of all")
