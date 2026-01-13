b8400bde0e615338863511c7a46a0972311633c2
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-13 22:37:05 +0530

    added questions and solved till4

---
+++ loop.py
@@ -0,0 +1,51 @@
+"""
+1. Write a program to print multiplication table of a given number using for loop.
+2. Write a program to greet all the person names stored in a list ‘l’ and which starts
+with S.
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+3. Attempt problem 1 using while loop.
+4. Write a program to find whether a given number is prime or not.
+5. Write a program to find the sum of first n natural numbers using while loop.
+6. Write a program to calculate the factorial of a given number using for loop.
+7. Write a program to print the following star pattern.
+ *
+***
+***** for n = 3
+8. Write a program to print the following star pattern:
+*
+**
+*** for n = 3
+9. Write a program to print the following star pattern.
+* * *
+* * for n = 3
+* * *
+10. Write a program to print multiplication table of n using for loops in reversed
+order.
+"""
+
+#1. Write a program to print multiplication table of a given number using for loop.
+n = int(input("enter a number :"))
+for i in range(1,11):
+    a = n*i
+    print(f"{2}*{i}={a}")
+
+#2Write a program to greet all the person names stored in a list ‘l’ and which starts with S.
+names = ["omii","sahil","sekhar sir"]
+for name in names:
+    if (name.startswith("s")):
+        print(f"Greetings, {name}")
+
+#3. Attempt problem 1 using while loop.
+n = int(input("enter a number :"))
+i = 0
+a = 0
+while (i<11):
+    print(f"{2}*{i}={a}")
+    i = i+1
+
+#4. Write a program to find whether a given number is prime or not.
+n = int(input("enter a number :"))
+for i in range (2,n):
+    if (n % i) == 0:
+        print("not prime")
+    break; 
