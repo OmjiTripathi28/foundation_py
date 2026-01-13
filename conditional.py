2b3823c7a5fd4502f3969a4421c231ed37d114c6
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-13 20:12:54 +0530

    solved6&7 and completed

--- conditional.py
+++ conditional.py
@@ -86,3 +86,32 @@ if name in names:
     print("present")
 else:
     print("not present")
+
+"""6Write a program to calculate the grade of a student from his marks from the
+following scheme:
+90 – 100 => Ex
+80 – 90 => A
+70 – 80 => B
+60 – 70 =>C
+50 – 60 => D
+<50 => F"""
+marks = int(input("enter your marks: "))
+if marks>90 and marks<100 :
+    print("Ex")
+elif marks>80 and marks<90 :
+    print("A")
+elif marks>70 and marks<80 :
+    print("B")
+elif marks>60 and marks<70 :
+    print("C")
+elif marks>50 and marks<60 :
+    print("D")
+else:
+    print("F")
+
+#7. Write a program to find out whether a given post is talking about “omii” or not
+post = input("enter your post: ")
+if "omii".lower() in post.lower() :
+    print("Talking about omii")
+else:
+    print("not talking about omii")
