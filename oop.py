9ab3788978bfc77263bad695dee65e6a8fea7b00
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-25 09:43:27 +0530

    sol4

--- oop.py
+++ oop.py
@@ -49,3 +49,27 @@ class demo:
 
 b = demo()
 b.a = 0 
+print(b.a)
+print(demo.a)
+
+"""4. Add a static method in problem 2, to greet the user with hello."""
+class calculator: 
+    
+    def __init__(self, n):
+        self.n = n
+    def square(self):
+        print(self.n*self.n)
+    def cube(self):
+        print(self.n*self.n*self.n)
+    def squareroot(self):
+        print(self.n**1/2)
+    @staticmethod 
+    def greet():
+        print("Hello user")
+    
+c = calculator(20)
+c.greet()
+c.square()
+c.cube()
+c.squareroot()
+
